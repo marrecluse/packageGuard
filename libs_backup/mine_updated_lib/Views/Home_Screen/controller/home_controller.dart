2aec219b867cec6b5aef04b7865b58e45104ed19
marrecluse <danijakhar11@gmail.com>
2024-01-23 19:14:32 +0500

    UI fixes Facebook Auth

--- libs_backup/mine_updated_lib/Views/Home_Screen/controller/home_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-
-class HomeController extends GetxController {
-  List<RxBool> isTappedList = [];
-
-  void enableIsArmed(int index) {
-    isTappedList[index].value = !isTappedList[index].value;
-  }
-}
